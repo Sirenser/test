306ab5eb87424a388b64dfe0d53039a398cb2170
Max <maksim.gurov.acc@gmail.com>
2021-03-20 20:24:57 +0400

    ВТФ НЕПОН В JS

--- jscode.js
+++ jscode.js
@@ -1,12 +1,36 @@
 'use strict'
 
-
-alert(1);
-
 //let quest = confirm("suck?");
 
 //console.log(quest);
 
-let quest = prompt("suck?", "yes");
+//let quest = prompt("suck?", "yes");
 
-console.log(typeof(quest));
+//console.log(typeof(quest));
+
+let money = +prompt("Vash bugje?");
+
+//let time = prompt("Vvedite daty v formate YYYY-MM-DD");
+
+let question1 = prompt("Vvedite obyazatel'nyu stat'u rashodov v etom mesyace")
+
+let question2 = +prompt("Vo skol'ko oboidetsya?")
+
+let appData = {
+    budjet: money,
+    timeData: time,
+    expenses: {
+      question1 : question2  
+    },
+    optionalExpenses: {
+
+    },
+    income: null,
+    savings: false
+};
+
+alert("Budjet na 1 den' sostavit " + (budjet/appData.expenses.question2));
+
+console.log(typeof(appData.budjet));
+console.log(typeof(appData.expenses.question2));
+//console.log(appData.budjet);
